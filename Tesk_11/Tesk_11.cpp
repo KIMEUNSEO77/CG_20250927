2b5c45c5c80894fc87d8d29e68db96a7213e1da2
eunseo <2023180012@tukorea.ac.kr>
2025-09-28 22:52:46 +0900

    기본코드

---
+++ Tesk_11/Tesk_11.cpp
@@ -0,0 +1,146 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <glew.h>
+#include <freeglut.h>
+#include <freeglut_ext.h> 
+void make_vertexShaders();
+void make_fragmentShaders();
+GLuint make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+GLint width, height;
+GLuint shaderProgramID;
+GLuint vertexShader;
+GLuint fragmentShader;
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb");
+	if (!fptr)
+		return NULL;
+	fseek(fptr, 0, SEEK_END);
+	length = ftell(fptr);
+	buf = (char*)malloc(length + 1);
+	fseek(fptr, 0, SEEK_SET);
+	fread(buf, length, 1, fptr);
+	fclose(fptr);
+	buf[length] = 0;
+	return buf;
+	// Open file for reading 
+	// Return NULL on failure 
+	// Seek to the end of the file 
+	// Find out how many bytes into the file we are 
+	// Allocate a buffer for the entire length of the file and a null terminator 
+	// Go back to the beginning of the file 
+	// Read the contents of the file in to the buffer 
+	// Close the file 
+	// Null terminator 
+	// Return the buffer 
+}
+
+void main(int argc, char** argv)
+{
+	width = 500;
+	height = 500;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Test_glsl");
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	make_vertexShaders();
+	make_fragmentShaders();
+	shaderProgramID = make_shaderProgram();
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+void make_vertexShaders()
+{
+	GLchar* vertexSource;
+	vertexSource = filetobuf("vertex.glsl");
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "Error: vertex shader            \n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	GLchar* fragmentSource;
+	fragmentSource = filetobuf("fragment.glsl");
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: frag_shader            \n" << errorLog << std::endl;
+		return;
+	}
+}
+
+GLuint make_shaderProgram()
+{
+	GLint result;
+	GLchar* errorLog = NULL;
+	GLuint shaderID;
+	shaderID = glCreateProgram();
+
+	glAttachShader(shaderID, vertexShader);
+	glAttachShader(shaderID, fragmentShader);
+
+	glLinkProgram(shaderID);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glGetProgramiv(shaderID, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
+		std::cerr << "ERROR: shader program          \n" << errorLog << std::endl;
+		return false;
+	}
+	glUseProgram(shaderID);
+	return shaderID;
+}
+
+GLvoid drawScene()
+{
+	GLfloat rColor, gColor, bColor;
+	rColor = gColor = 0.0;
+	bColor = 1.0;
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(shaderProgramID);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
